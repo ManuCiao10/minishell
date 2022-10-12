da5c8456266b55cfba803efe5680eb71f12f2cb8
Emanuele Ardinghi <eardingh@c2r3p7.42quebec.com>
2022-10-12 11:02:09 -0400

    env

--- src/minishell.c
+++ src/minishell.c
@@ -2,34 +2,35 @@
 
 t_data *get_data()
 {
-	t_data *data;
+	static t_data *data = NULL;
 
-	data = malloc(sizeof(t_data));
-	if (!data)
-		return (NULL);
+	if(data == NULL)
+		data = (t_data *)malloc(sizeof(t_data));
 	return (data);
 }
 
-void init_data(t_data *data)
+void init_data(char *envp[])
 {
+	t_data	*data;
+
+	data = get_data();
 	data->line = NULL;
-	data->env = NULL;
+	// data->env = init_env(envp);
 	data->cmd = 0;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char *argv[], char *envp[])
 {
 	t_data *data;
 
 	(void)argc;
 	(void)argv;
 	data = get_data();
-	init_data(data);
+	init_data(envp);
 	while(1)
 	{
 		data->line = readline("\033[0;36m\033[1mminishell ▸ \033[0m");
-		if (!data->line)
-			break;
+		printf("line: %s", data->line);
 	}
 
 
